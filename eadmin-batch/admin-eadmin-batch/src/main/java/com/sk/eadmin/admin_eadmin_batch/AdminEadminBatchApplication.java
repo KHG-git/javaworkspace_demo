8b50cbb9c3a1d82c524b7963905c4e5dc4c4b3b3
KangHyunGoo <kang.hyungoo@gmail.com>
2025-06-10 16:30:53 +0900

    Auto commit - 2025-06-10 16:30:51.31

---
+++ eadmin-batch/admin-eadmin-batch/src/main/java/com/sk/eadmin/admin_eadmin_batch/AdminEadminBatchApplication.java
@@ -0,0 +1,13 @@
+package com.sk.eadmin.admin_eadmin_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminEadminBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminEadminBatchApplication.class, args);
+	}
+
+}
